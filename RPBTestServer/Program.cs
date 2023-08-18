1ae6424b64cabf13cb0d73c69036c017b1f10704
Alexander Hartmann-Kanter <devkanter@gmail.com>
2023-08-18 10:22:34 +0200

    Add project files.

---
+++ RPBTestServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPBTestServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
